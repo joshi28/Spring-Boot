0fc5baa414c671a150afa8e6e7b7180467da1e7e
Jinal <jinal.joshi150@gmail.com>
2019-03-03 00:51:16 -0500

    Spring boot Disabling Auto configure

---
+++ SpringBootDisablingAutoConfigure/src/main/java/com/springboot/starter/SpringBootDisablingAutoConfigureApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class SpringBootDisablingAutoConfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDisablingAutoConfigureApplication.class, args);
+	}
+
+}
